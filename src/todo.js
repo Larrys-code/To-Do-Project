dcee2f5c47283484edb081f857c1640f0ceada4b
Larry <38928644+Larrys-code@users.noreply.github.com>
2023-05-04 11:29:41 +0100

    Build out basic ToDo Class

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,41 @@
-class ToDo {
-  constructor() {}
+export default class ToDo {
+  constructor(
+    name,
+    id,
+    description,
+    due = false,
+    priority = 0,
+    checklist = []
+  ) {
+    this.name = name;
+    this.id = id;
+    this.description = description;
+    this.due = due;
+    this.priority = priority;
+    this.checklist = checklist;
+  }
+
+  editValue = (key, settings) => {
+    switch (key) {
+      case "name":
+        this.name = settings;
+        break;
+      case "id":
+        this.id = settings;
+        break;
+      case "description":
+        this.description = settings;
+        break;
+      case "due":
+        this.due = settings;
+        break;
+      case "priority":
+        this.priority = settings;
+        break;
+      case "checklist":
+        this.checklist = settings;
+        break;
+      default:
+    }
+  };
 }
